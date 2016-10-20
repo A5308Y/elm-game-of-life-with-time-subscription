c9da8fd5c4b34f8cb85ed67cf067df0295629ff5
Andreas Hellwig <a5308y@gmail.com>
2016-10-20 17:22:34 +0200

    fixing specs

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -10,9 +10,9 @@ all : Test
 all =
     describe "Cell"
         [ test "neighbours finds positions with difference in one coordinate" <|
-            \() -> Expect.equal [ ( 1, 4 ), ( 2, 3 ) ] (neighbours [ ( 2, 3 ), ( 1, 4 ), ( 5, 5 ) ] ( 1, 3 ))
+            \() -> Expect.equal (Set.fromList [ ( 1, 4 ), ( 2, 3 ) ]) (neighbours (Set.fromList [ ( 2, 3 ), ( 1, 4 ), ( 5, 5 ) ]) ( 1, 3 ))
         , test "neighbours does not include istelf" <|
-            \() -> Expect.equal [ ( 1, 3 ) ] (neighbours [ ( 1, 2 ), ( 1, 3 ) ] ( 1, 2 ))
+            \() -> Expect.equal (Set.fromList [ ( 1, 3 ) ]) (neighbours (Set.fromList [ ( 1, 2 ), ( 1, 3 ) ]) ( 1, 2 ))
         , test "update creates new cells for positions with three neighbours" <|
             \() ->
                 Expect.equal
